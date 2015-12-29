42e24de9bd7dcb0297b7bded913a0b54c1c93770
shreyap26 <shreyap@seas.upenn.edu>
2015-12-29 12:41:02 -0500

    organizing 2

--- scripts/pdf_to_xml_v2.py
+++ scripts/pdf_to_xml_v2.py
@@ -2,11 +2,12 @@ import os
 from utilities import getFiles
 
 def pdfToXML(inputfilepath="", outdir=""):
-	file_name_last_index = inputfilepath.index(".")
+	file_name_last_index = inputfilepath.rfind(".")
 	file_name_first_index = inputfilepath.rfind("/")
 	filename = inputfilepath[file_name_first_index: file_name_last_index]
+	print filename
 	#print outdir
-	command =  "pdftohtml -xml" + " " + inputfilepath + " " + outdir + filename+".xml"
+	command =  "pdftohtml -xml" + " " + inputfilepath.replace(' ', '\ ') + " " + outdir + filename.replace(' ', '\ ')+".xml"
 	#print command
 	os.system(command)
 
@@ -14,11 +15,13 @@ def pdfToXML(inputfilepath="", outdir=""):
 def convertAll(dirpath="", outdir=""):
 	filepaths = getFiles(dirpath)
 	for filepath in filepaths:
+		print filepath
 		pdfToXML(filepath, outdir)
 
 if __name__ == "__main__":
 	#filepath = "/home/shreya/Wharton/1.pdf"
 	#pdfToXML(filepath)
+
 	dirpath = "/home/shreya/Wharton/PDF"
 	outdir = "/home/shreya/Wharton/XML"
 	convertAll(dirpath, outdir)
