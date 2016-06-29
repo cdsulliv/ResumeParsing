942e3855eec21a60f3be9fb4ecd46a4e952fd0f5
JamesCreynolds <jcutreynolds@gmail.com>
2016-06-29 12:31:08 -0400

    Update Sovren Parser.py

--- Current/Sovren Parser.py
+++ Current/Sovren Parser.py
@@ -14,11 +14,14 @@ import string
 from HeaderSplitting import ParseText
 
 ############   Set variables to local machine   #############
-xml_loc = "/Users/cutlerreynolds/Dropbox/Resume-audit/Scraping Project/Sovren/OUTPUT/"
+xml_loc = "/Users/cutlerreynolds/Dropbox/Resume-audit/Scraping Project/Sovren/Penn Clubs Output/All xml files/"
+folders = ["GM Output", "MARC Output", "MUSE Output", "Networking Lunch Output", "Wharton Alliance Output", "WUCC Output"]
 fileset = [xml_loc + f for f in os.listdir(xml_loc)]
 filenames = [f for f in os.listdir(xml_loc)]
 outputloc = "/Users/cutlerreynolds/Dropbox/Resume-audit/Scraping Project/Output"
 outputfile = outputloc+"/Sovren-Parse-Data.csv"
+Arts = 0
+Sciences = 0
 #############################################################
 
 def exists(soup):
@@ -33,29 +36,41 @@ def find_Edu(soup):
     School_Name = exists(soup.find("schoolname"))
     Degree = exists(soup.find("degreemajor"))
     GPA = exists(soup.find("measurevalue"))
+    Degree_name = exists(soup.find("degreename"))
+
     
     if "concentration" in Degree.lower() or "intended" in Degree.lower():
         Degree = exists(soup.find("degreename"))
+    
+    if "in" in Degree_name:
+        Degree = Degree_name
 
     #print [School_Name.get_text(), Degree.get_text(), GPA]
     return [School_Name, Degree, GPA]
 
 
-#Returns a list of Work Experience information [[Job1Company, Job1Position, Internship], [Job2Company, Job2Position, Internship]]
+#Returns a list of Work Experience information [[Job1Company, Job1Position], [Job2Company, Job2Position]]
 def find_workExp(soup):
     jobList = []
     jobNumber = len(soup.find_all('positionhistoryuserarea'))
-    allOrgs = soup.find_all('employerorg')
+    orgNames = soup.find_all('employerorg')
 
-    for org in allOrgs:
-        PositionNames = org.find_all('title')
-        CompanyName = exists(org.find('employerorgname'))
-        
-        for position in PositionNames:
-            jobList.append([CompanyName, exists(position)])
+    for organization in orgNames:
+        allPositions = organization.find_all('positionhistory')
+        for position in allPositions:
+            PositionName = exists(position.find('title'))
+            CompanyName = exists(position.find('organizationname'))
 
-    if jobNumber < 5:
-        while len(jobList) < 5:
+            if CompanyName.strip('') == '':
+                CompanyName = exists(organization.find('employerorgname'))
+                #print "Name is: " + CompanyName
+                if CompanyName.strip('') == '':
+                    CompanyName = 'Not Listed'
+
+            jobList.append([CompanyName, PositionName])
+
+    if jobNumber < 10:
+        while len(jobList) < 10:
             jobList.append(['Not Listed', 'Not Listed'])
 
     #print jobList
@@ -66,7 +81,7 @@ def find_dates(soup):
     startDates = []
     endDates = []
 
-    jobs = soup.find_all("employerorg")
+    jobs = soup.find_all("positionhistory")
 
     for job in jobs:
         startDates.append(exists(job.find("startdate")))
@@ -74,8 +89,8 @@ def find_dates(soup):
 
     dateList = zip(startDates, endDates)
 
-    if len(dateList) < 5:
-        while len(dateList) < 5:
+    if len(dateList) < 10:
+        while len(dateList) < 10:
             dateList.append(('Not Listed', 'Not Listed'))
 
     return dateList
@@ -84,15 +99,15 @@ def find_descriptions(soup):
     
     descriptions = []
     
-    jobs = soup.find_all("employerorg")
+    jobs = soup.find_all("positionhistory")
     
     # Commented Out for now due to ascii issues
     for job in jobs:
         description = job.find("description")
         descriptions.append(description.get_text().encode('ascii', 'replace'))
     
-    if len(descriptions) < 5:
-        while len(descriptions) < 5:
+    if len(descriptions) < 10:
+        while len(descriptions) < 10:
             descriptions.append('Not Listed')
 
     return descriptions
@@ -121,40 +136,56 @@ def find_activities(soup):
 
     return [achiev_list, assoc_list]
 
-if __name__ == "__main__":
+def classify_majors(sample_major):
+    global Arts, Sciences
+    
+    col_majors = open("/Users/cutlerreynolds/Dropbox/Resume-audit/Scraping Project/CollegeMajors.txt", 'r')
+    c_major_list = col_majors.read().split(',')
 
+    eng_majors = open("/Users/cutlerreynolds/Dropbox/Resume-audit/Scraping Project/EngineeringMajors.txt", 'r')
+    e_major_list = eng_majors.read().split(',')
+
+    wha_majors = open("/Users/cutlerreynolds/Dropbox/Resume-audit/Scraping Project/WhartonMajors.txt", 'r')
+    w_major_list = wha_majors.read().split(',')
+    
+    all_majors = [c_major_list, e_major_list, w_major_list]
+    
+    for school in all_majors:
+        for major in school:
+            if major in sample_major:
+                if school == c_major_list or w_major_list:
+                    Arts = Arts + 1
+                    print major
+                elif school == e_major_list:
+                    Sciences = Sciences + 1
+                    print major
+
+if __name__ == "__main__":
+    
      # Get list of files for iteration
     XMLSet = [f for f in fileset if f.endswith(".xml")]
     XMLNames = [f for f in filenames if f.endswith(".xml")]
     assert len(XMLSet) == len(XMLNames) 
-
     xmlset = set()
-
+    
     with open (outputfile, 'w') as csvfile:
         csvwriter = csv.writer(csvfile, delimiter=',')
-        csvwriter.writerow(['FILENAME', 'SCHOOL', 'DEGREE', 'GPA', 'JOB 1 COMPANY', 'JOB 1 TITLE', 'JOB 1 DESCRIPTION', 'JOB 1 START DATE', 'JOB 1 END DATE', 'JOB 2 COMPANY', 'JOB 2 TITLE', 'JOB 2 DESCRIPTION', 'JOB 2 START DATE', 'JOB 2 END DATE', 'JOB 3 COMPANY', 'JOB 3 TITLE', 'JOB 3 DESCRIPTION', 'JOB 3 START DATE', 'JOB 3 END DATE', 'JOB 4 COMPANY', 'JOB 4 TITLE', 'JOB 4 DESCRIPTION', 'JOB 4 START DATE', 'JOB 4 END DATE', 'JOB 5 COMPANY', 'JOB 5 TITLE', 'JOB 5 DESCRIPTION', 'JOB 5 START DATE', 'JOB 5 END DATE', 'QUALIFICATIONS', 'ACHIEVMENTS', 'ASSOCIATIONS'])
-
+        csvwriter.writerow(['FILENAME', 'SCHOOL', 'DEGREE', 'GPA', 'JOBCOMPANY1', 'JOBTITLE1', 'JOBDESCRIPTION1', 'JOBSTARTDATE1', 'JOBENDDATE1', 'JOBCOMPANY2', 'JOBTITLE2', 'JOBDESCRIPTION2', 'JOBSTARTDATE2', 'JOBENDDATE2', 'JOBCOMPANY3', 'JOBTITLE3', 'JOBDESCRIPTION3', 'JOBSTARTDATE3', 'JOBENDDATE3', 'JOBCOMPANY4', 'JOBTITLE4', 'JOBDESCRIPTION4', 'JOBSTARTDATE4', 'JOBENDDATE4', 'JOBCOMPANY5', 'JOBTITLE5', 'JOBDESCRIPTION5', 'JOBSTARTDATE5', 'JOBENDDATE5', 'JOBCOMPANY6', 'JOBTITLE6', 'JOBDESCRIPTION6', 'JOBSTARTDATE6', 'JOBENDDATE6', 'JOBCOMPANY7', 'JOBTITLE7', 'JOBDESCRIPTION7', 'JOBSTARTDATE7', 'JOBENDDATE7', 'JOBCOMPANY8', 'JOBTITLE8', 'JOBDESCRIPTION8', 'JOBSTARTDATE8', 'JOBENDDATE8', 'JOBCOMPANY9', 'JOBTITLE9', 'JOBDESCRIPTION9', 'JOBSTARTDATE9', 'JOBENDDATE9', 'JOBCOMPANY10', 'JOBTITLE10', 'JOBDESCRIPTION10', 'JOBSTARTDATE10', 'JOBENDDATE10', 'QUALIFICATIONS', 'ACHIEVMENTS', 'ASSOCIATIONS'])
+    
         for xml_filename, xml_filepath in zip(XMLNames, XMLSet):
             print "filename: ", xml_filename
-
+    
             handler = open(xml_filepath).read()
-
+    
             soup = BeautifulSoup(handler, "html.parser")
-
+    
             Edu = find_Edu(soup)
             exp = find_workExp(soup)
             dates = find_dates(soup)
             dscrpt = find_descriptions(soup)
             qual = find_qualifications(soup)
             act = find_activities(soup)
-
-            row = [xml_filename, Edu[0], Edu[1], Edu[2], exp[0][0], exp[0][1], dscrpt[0], dates[0][0], dates[0][1], exp[1][0], exp[1][1], dscrpt[1], dates[1][0], dates[1][1], exp[2][0], exp[2][1], dscrpt[2], dates[2][0], dates[2][1], exp[3][0], exp[3][1], dscrpt[3], dates[3][0], dates[3][1], exp[4][0], exp[4][1], dates[4][0], dscrpt[4], dates[4][1], qual, act[0], act[1]]
-
+    
+            row = [xml_filename, Edu[0], Edu[1], Edu[2], exp[0][0], exp[0][1], dscrpt[0], dates[0][0], dates[0][1], exp[1][0], exp[1][1], dscrpt[1], dates[1][0], dates[1][1], exp[2][0], exp[2][1], dscrpt[2], dates[2][0], dates[2][1], exp[3][0], exp[3][1], dscrpt[3], dates[3][0], dates[3][1], exp[4][0], exp[4][1], dscrpt[4], dates[4][0], dates[4][1], exp[5][0], exp[5][1], dscrpt[5], dates[5][0], dates[5][1], exp[6][0], exp[6][1], dscrpt[6], dates[6][0], dates[6][1], exp[7][0], exp[7][1], dscrpt[7], dates[7][0], dates[7][1], exp[8][0], exp[8][1], dscrpt[8], dates[8][0], dates[8][1], exp[9][0], exp[9][1], dscrpt[9], dates[9][0], dates[9][1], qual, act[0], act[1]]
+    
             csvwriter.writerow(row)
-            
-            
-            
-            
-            
-            
-
